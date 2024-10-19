b84293f8a489c97227cebba6b03c63068e1bedf8
Azryy <doradokurt02@gmail.com>
2024-10-19 10:13:43 +0800

    Another commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,7 @@ const Header = () => {
   return (
     <div className='header'>
       <div className="header-contents">
-        <h2>Order your favorite dessert here</h2>
-        <p>Unforgettable Cakes for Unforgettable Moments: <br></br>The Delights of Kapalaran Bakery</p>
-        <button>View Menu</button>
+        
       </div>
     </div>
   )
